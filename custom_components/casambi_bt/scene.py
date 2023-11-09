62bd7311897e1b1bc28fdbab64dd309817a19ca8
Christopher Fenner <Christopher.Fenner@me.com>
2023-11-10 00:21:55 +0100

    adjust class names

--- custom_components/casambi_bt/scene.py
+++ custom_components/casambi_bt/scene.py
@@ -5,20 +5,17 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-from CasambiBt import Scene
+from CasambiBt import Scene as CasambiScene
 
-from . import CasambiApi
-
-from .const import (
-    DOMAIN,
-)
+from homeassistant.components.light import ATTR_BRIGHTNESS
+from homeassistant.components.scene import Scene as SceneEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from homeassistant.components.light import ATTR_BRIGHTNESS
-from homeassistant.components.scene import Scene as SceneEntity
+from . import CasambiApi
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,12 +27,12 @@ async def async_setup_entry(
 ) -> None:
     casa_api: CasambiApi = hass.data[DOMAIN][config_entry.entry_id]
 
-    scenes = [CasambiScene(casa_api, scene) for scene in casa_api.get_scenes()]
+    scenes = [CasambiSceneEntity(casa_api, scene) for scene in casa_api.get_scenes()]
     async_add_entities(scenes)
 
 
-class CasambiScene(SceneEntity):
-    def __init__(self, api: CasambiApi, scene: Scene) -> None:
+class CasambiSceneEntity(SceneEntity):
+    def __init__(self, api: CasambiApi, scene: CasambiScene) -> None:
         self._api = api
         self.scene = scene
 
@@ -54,6 +51,6 @@ class CasambiScene(SceneEntity):
         )
 
     async def async_activate(self, **kwargs: Any) -> None:
-        _LOGGER.info(f"Switching to scene {self.name}")
+        _LOGGER.info("Switching to scene %s", self.name)
         brightness = kwargs.get(ATTR_BRIGHTNESS, 0xFF)
         await self._api.casa.switchToScene(self.scene, brightness)
